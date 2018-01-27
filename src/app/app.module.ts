2172ea6432c03eb529de1b1b61809e4fbb299af3
rrpearcy <ray@pearcynet.com>
2018-01-27 01:42:00 -0600

    first commit with clean setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { EventListComponent } from './event-list/event-list.component';
+import { EventDetailComponent } from './event-detail/event-detail.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EventListComponent,
+    EventDetailComponent
   ],
   imports: [
     BrowserModule,
